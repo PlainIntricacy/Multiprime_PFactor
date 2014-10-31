354498bcc34de4edef09454c601be9dd621982a3
Tudor Laptes <tudorlaptes@gmail.com>
2014-10-31 10:41:29 -0400

    Second commit

--- src/multiprime/factor/Main.java
+++ src/multiprime/factor/Main.java
@@ -1,12 +1,13 @@
 /**
  * Largest multiprime prime factor calculator.
+ *
  * Input: upper limit number
- * Output: largest prime factor of the product of all prime numbers below the given upper limit
+ * Output: largest prime factor of the sum of all prime numbers below the given upper limit
+ * Also displays the above mentioned prime sum for accuracy checking
  */
 
 package multiprime.factor;
 import java.util.Scanner;
-import java.math.*;
 
 /**
  * Author - todyerutz@plainintricacy.wordpress.com
@@ -20,25 +21,23 @@ public class Main {
         System.out.println("Enter the upper limit to calculate:");
         int input = in.nextInt();
         in.close();
-        System.out.println("Largest multiprime prime factor: " + getFact(calc_mp(input)));
+        System.out.println("Sum of primes below " + input + ": " + calc_mp(input));
+        System.out.println("Largest prime factor of " + calc_mp(input) + ": " + getFact(calc_mp(input)));
 
     }
 
-    public static long calc_mp(int x){
-    //Calculates the multiprime product below the given limit number
-        long mp = 1;
+    public static int calc_mp(int x){
+    //Calculates the sum of all primes below the given limit number
+        int mp = 0;
         if(x>=2){
-            mp*=2;
+            mp+=2;
         }
         for(int i=3; i<=x; i+=2){
             if(primality(i)){
-                mp*=i;
+                mp+=i;
             }
         }
-        if(mp>=2){
-            return mp;
-        }
-        return 0;
+        return mp;
     }
 
     public static boolean primality(int q) {
@@ -47,29 +46,33 @@ public class Main {
             return true;
         } else if (q % 2 == 0 && q != 2) {
             return false;
-        } else
-            for (int i = 3; i < Math.sqrt(q); i += 2) {
+        } else {
+            for (int i = 3; i <= Math.sqrt(q); i += 2) {
                 if (q % i == 0) {
                     return false;
                 }
             }
+        }
         return true;
     }
 
-    public static int getFact(long x){
+    public static int getFact(int x){
     //Calculates the largest prime factor of a given number
-        int result=0;
-        if(x%2==0){
-            result=2;
-        }
-        for(int i=3; i<Math.sqrt(x); i+=2){
-            if(x%i==0&&primality(i)){
-                if(i>result) {
+        if(primality(x)){
+            System.out.println("The multiprime sum, " + x + ", is itself a prime number.");
+            return 0;
+        }else {
+            int result = 0;
+            if(x%2==0){
+                result=2;
+            }
+            for(int i=3; i<=x/2; i+=2){
+                if(primality(i)&&x%i==0){
                     result = i;
                 }
             }
+            return result;
         }
-        return result;
     }
 
 }
